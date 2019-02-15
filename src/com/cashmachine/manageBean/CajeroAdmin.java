4dd5e8a2f8f0249b34efc563f9cd4e50966ee27e
juan_pablo <juanp0021@yahoo.com>
2019-02-14 20:47:36 -0500

    version con griview

--- src/com/cashmachine/manageBean/CajeroAdmin.java
+++ src/com/cashmachine/manageBean/CajeroAdmin.java
@@ -1,10 +1,15 @@
 package com.cashmachine.manageBean;
 import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+
 import net.bytebuddy.asm.Advice.This;
+
 import com.cashmachine.entity.AdminCajero;
 import com.cashmachine.services.CashMachineService;
 
@@ -16,6 +21,18 @@ public class CajeroAdmin implements Serializable {
 	
 	//  SE DOCUMENTA EL ATRIBUTO
 	private AdminCajero admincajero;
+	private boolean obligatorio;
+	private CashMachineService cashMachineService;
+	private ArrayList<AdminCajero> billetes;
+	
+	public ArrayList<AdminCajero> getBilletes() {
+		return billetes;
+	}
+
+	public void setBilletes(ArrayList<AdminCajero> billetes) {
+		this.billetes = billetes;
+	}
+
 	public AdminCajero getAdmincajero() {
 		return admincajero;
 	}
@@ -24,19 +41,16 @@ public class CajeroAdmin implements Serializable {
 		this.admincajero = admincajero;
 	}
 
+	
 	public CashMachineService getCashMachineService() {
 		return cashMachineService;
 	}
 
+	
 	public void setCashMachineService(CashMachineService cashMachineService) {
 		this.cashMachineService = cashMachineService;
 	}
 
-
-	private boolean obligatorio;
-	private CashMachineService cashMachineService;
-	
-	
 	public boolean isObligatorio() {
 		return obligatorio;
 	}
@@ -52,14 +66,20 @@ public class CajeroAdmin implements Serializable {
 	}
 
 	// se inicializa los objetos en caso de ser null
+	@PostConstruct
 	public void  init (){
-		
 		this.admincajero = new AdminCajero();
 		this.cashMachineService = new CashMachineService();
 		this.obligatorio = true;
-		
+		loadBilletes ();
 	}
 
+	
+	public void loadBilletes (){
+		
+		this.billetes = cashMachineService.listarBilletes();
+		
+	}
 		
 	// SE DOCUMENTA EL METODO
 	public void  guardarCantidad(){
@@ -67,21 +87,21 @@ public class CajeroAdmin implements Serializable {
 		System.out.println(" GUARDO!!!!!"+this.admincajero.getCantidad());
 		
 		
-		boolean resultado;
+		AdminCajero resultadoAdminCajero;
 		try {
-			resultado = cashMachineService.guardar(this.admincajero);
 			
-			if (resultado){
+			resultadoAdminCajero = cashMachineService.guardar(this.admincajero);
+			
+			if (resultadoAdminCajero != null){
 				// una vez guarda limpia  las variables
-				
 			     this.admincajero = new AdminCajero();
 				 FacesContext context = FacesContext.getCurrentInstance();
 		         context.addMessage(null, new FacesMessage("Resultado",  "se guardo el registro exitosamente.") );
+		         this.billetes.add(resultadoAdminCajero);
+		         
 			}else{
-			 	
 			     FacesContext context = FacesContext.getCurrentInstance();
 		         context.addMessage(null, new FacesMessage("Resultado",  "error al guardar.") );
-				
 			}
 			
 			
@@ -95,5 +115,10 @@ public class CajeroAdmin implements Serializable {
 
 	}
 	
-
+	public void eliminar(AdminCajero adminCajero){
+		
+		cashMachineService.eliminar(adminCajero);
+		loadBilletes ();
+		
+	}
 }
--- src/com/cashmachine/services/CashMachineService.java
+++ src/com/cashmachine/services/CashMachineService.java
@@ -1,11 +1,13 @@
 package com.cashmachine.services;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 import com.cashmachine.entity.AdminCajero;
+import com.cashmachine.manageBean.CajeroAdmin;
 
 
 
@@ -22,42 +24,72 @@ public class CashMachineService {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public boolean guardar(AdminCajero adminCajero) throws Exception{
-		
-		// aca va el persist
-		/*
 	
-		if (entityManager.isOpen()){
-			entityManager.getTransaction().begin();
-			entityManager.persist(adminCajero);
-			entityManager.getTransaction().commit();
+	public AdminCajero guardar(AdminCajero adminCajero) throws Exception{
 		
-		}
-		*/
 		
 		entityManager.getTransaction().begin();
 		try {
-		    //do something with your database
-			System.out.println("id------"+adminCajero.getId());
-			adminCajero = entityManager.merge(adminCajero);
-			
-			System.out.println("id>>>>>>"+adminCajero.getId());
-			
-			
-			
-			entityManager.getTransaction().commit();
-		} 
-		finally {
-		   if (entityManager.getTransaction().isActive())
+				System.out.println("id------"+adminCajero.getId());
+				adminCajero = entityManager.merge(adminCajero);
+				System.out.println("id>>>>>>"+adminCajero.getId());
+				entityManager.getTransaction().commit();
+			    return adminCajero;	
+		}catch(Exception err){
+			if (entityManager.getTransaction().isActive()){
 			   entityManager.getTransaction().rollback(); 
-		}
-		
+			}
+		} 
+	
+		return null;
+	}// fin de guardar
+
+
+	@SuppressWarnings("unchecked")
+	public ArrayList<AdminCajero> listarBilletes() {
+		// TODO Auto-generated method stub
+		entityManager.getTransaction().begin();
+		try {
+				
+			ArrayList<AdminCajero> adminCajeroList = new ArrayList<AdminCajero>();
+				
+			adminCajeroList = (ArrayList<AdminCajero>) entityManager.createQuery(
+			            "SELECT p FROM AdminCajero p").getResultList();
 			
+				entityManager.getTransaction().commit();
+			    
 				
+				if (adminCajeroList == null){
+					adminCajeroList= new ArrayList<AdminCajero>();
+				}
 				
-		//entityManager.close();
+				return adminCajeroList;
+			    
+			    
+		}catch(Exception err){
+			if (entityManager.getTransaction().isActive()){
+			   entityManager.getTransaction().rollback(); 
+			}
+		} 
 		
-		return false;
+		
+		return null;
 	}
 
+	
+	public int eliminar(AdminCajero adminCajero){
+		
+		entityManager.getTransaction().begin();
+		try {
+				entityManager.remove(adminCajero);
+				entityManager.getTransaction().commit();
+			    return 1;	
+		}catch(Exception err){
+			if (entityManager.getTransaction().isActive()){
+			   entityManager.getTransaction().rollback(); 
+			}
+			
+			return 0;
+		} 
+	}
 }
